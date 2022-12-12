14213f4dcc6d5e56e36ff3716763df90a56de5ef
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-12 15:02:06 -0800

    Completed login and Register responsiveness

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -167,57 +167,57 @@ function Register() {
 
     return (
         <div className='flex justify-center items-center py-16'>
-           <div className='bg-white w-[400px] h-[500px] shadow-2xl flex flex-col items-center rounded-2xl p-5'>
-            <h2 className='text-2xl font-bold'>Welcome to FoodCity</h2>
-            <p>Register your account</p>
-            <form className='w-full mt-4 relative' onSubmit={handleRegister}>
-                <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md my-4'>
-                    <input ref={usernameInput} className='w-[90%] outline-none p-2' type="text" required />
-                    <p ref={p0} className='absolute top-1 text-gray-500 bg-white left-3'>Username</p>
-                    <FiUser />
-                </div>
-                <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md my-4'>
-                    <input ref={emailInput} className='w-[90%] outline-none p-2' type="text" required />
-                    <p ref={p1} className='absolute top-1 text-gray-500 bg-white left-3'>Email</p>
-                    <AiOutlineMail />
-                </div>
-                <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md'>
-                    <input type="password" className='w-[90%] outline-none p-2' ref={passwordInput} required />
-                    <p ref={p2} className='absolute top-1 text-gray-500 bg-white left-3'>Password</p>
-                    { seePass ? <FiEyeOff className='cursor-pointer' onClick={hidePassword} />
-                        : <IoEyeOutline className='cursor-pointer' onClick={seePassword} />
-                    }
-                </div>
-
-                <input type="file" id="file" className='hidden' />
-                <label className='flex items-center justify-center mt-5' htmlFor="file">
-                    <img className='w-10 mx-3' src={Add} alt="choose file" />
-                    <p>Choose your avatar</p>
-                </label>
-
-                {
-                    uploadImgErr && <div onAnimationEnd={animationEnd} className='upload bg-orange-200 p-5 w-[250px] m-auto rounded-xl text-center text-textColor'>
-                     <p>Upload an Image</p>
+            <div className='bg-white relative w-[400px] h-[500px] shadow-2xl flex flex-col items-center rounded-2xl p-5'>
+                <h2 className='text-2xl font-bold'>Welcome to FoodCity</h2>
+                <p>Register your account</p>
+                <form className='w-full mt-4 relative' onSubmit={handleRegister}>
+                    <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md my-4'>
+                        <input ref={usernameInput} className='w-[90%] outline-none p-2' type="text" required />
+                        <p ref={p0} className='absolute top-1 text-gray-500 bg-white left-3'>Username</p>
+                        <FiUser />
                     </div>
-                }
+                    <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md my-4'>
+                        <input ref={emailInput} className='w-[90%] outline-none p-2' type="text" required />
+                        <p ref={p1} className='absolute top-1 text-gray-500 bg-white left-3'>Email</p>
+                        <AiOutlineMail />
+                    </div>
+                    <div className='flex justify-between items-center pr-3 relative w-[85%] m-auto border border-gray rounded-md'>
+                        <input type="password" className='w-[90%] outline-none p-2' ref={passwordInput} required />
+                        <p ref={p2} className='absolute top-1 text-gray-500 bg-white left-3'>Password</p>
+                        { seePass ? <FiEyeOff className='cursor-pointer' onClick={hidePassword} />
+                            : <IoEyeOutline className='cursor-pointer' onClick={seePassword} />
+                        }
+                    </div>
 
-                {
-                    emailTaken && <p className='text-center mt-2 text-red-600'>Email has been taken by other user.</p>
-                }
+                    <input type="file" id="file" className='hidden' />
+                    <label className='flex items-center justify-center mt-5' htmlFor="file">
+                        <img className='w-10 mx-3' src={Add} alt="choose file" />
+                        <p>Choose your avatar</p>
+                    </label>
 
-                {
-                    err && <p className='text-center mt-2 text-red-600'>Something went wrong, try again.</p>
-                }
-                
-                <div className='flex justify-center items-center my-6'>
-                    <button className='bg-orange-400 text-white w-[200px] p-2 rounded-xl text-center'>Register</button>
-                </div>
+                    {
+                        uploadImgErr && <div onAnimationEnd={animationEnd} className='upload bg-orange-200 p-5 w-[250px] m-auto rounded-xl text-center text-textColor'>
+                        <p>Upload an Image</p>
+                        </div>
+                    }
 
-                <div className='text-center text-sm'>
-                    <p>Already have an account? <Link to="/login" className='text-orange-400'>Login</Link></p>
-                </div>
-            </form>
-           </div>
+                    {
+                        emailTaken && <p className='text-center mt-2 text-red-600'>Email has been taken by other user.</p>
+                    }
+
+                    {
+                        err && <p className='text-center mt-2 text-red-600'>Something went wrong, try again.</p>
+                    }
+                    
+                    <div className='flex justify-center items-center my-6'>
+                        <button className='bg-orange-400 text-white w-[200px] p-2 rounded-xl text-center'>Register</button>
+                    </div>
+
+                    <div className='text-center text-sm'>
+                        <p>Already have an account? <Link to="/login" className='text-orange-400'>Login</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
