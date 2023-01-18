2e020d5b82ba4404977ada89db75637b33666aea
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-18 13:41:23 -0400

    added signup function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,19 @@ function createToken(_id) {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 }
 
+export async function loginUser(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Users.login(email, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
 export async function signupUser(req, res) {
   const { email, password } = req.body;
 
