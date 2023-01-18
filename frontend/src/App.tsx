3aa6ef7422b5f82aa205bce169fc55b1b983f022
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-17 20:21:31 -0400

    added login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 function App() {
 
   return (
     <div>
-      
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
