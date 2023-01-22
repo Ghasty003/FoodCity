3819e73017f117dc04cf82d46d0bc19cb0a55aa2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-22 02:03:59 -0400

    added products component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import ProductHeader from '../components/ProductHeader';
+import Products from '../components/Products';
 
 function Home() {
     return (
         <div>
             <NavBar />
             <ProductHeader />
+            <Products />
         </div>
     );
 }
