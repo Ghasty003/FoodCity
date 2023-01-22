f688c876096a82c1c4413ae9d7247fe9364aed9f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-22 02:03:04 -0400

    added codes

--- frontend/src/components/Products.tsx
+++ frontend/src/components/Products.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function Products() {
+
     return (
-        <div>
-            
+        <div className='mt-32'>
+            <div>
+                <h2 className='capitalize text-headingColor font-bold text-2xl'>our fresh & healthy fruits</h2>
+                <p className='w-32 h-1 rounded-xl bg-orange-500'></p>
+            </div>
+
+            <div className='w-[100%] h-48 relative mt-16 overflow-x-auto'>
+                <div> </div>
+            </div>
         </div>
     );
 }
