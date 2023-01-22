aeb2330be9f77f5066f0ef35e128c14fb8d80e63
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-22 01:53:01 -0400

    added bg image

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import ProductHeader from '../components/ProductHeader';
 
 function Home() {
     return (
         <div>
             <NavBar />
+            <ProductHeader />
         </div>
     );
 }
