b987d34a4fde7628c691a32dcdb1c269dd6ec0f1
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-22 02:07:24 -0400

    added header data

---
+++ frontend/src/data/header-data.ts
@@ -0,0 +1,37 @@
+import item1 from "../assets/f9.png";
+import item2 from "../assets/f8.png";
+import item3 from "../assets/f10.png";
+import item4 from "../assets/f4.png";
+
+const lists = [
+  {
+    id: 0,
+    title: "Banana",
+    description: "100 Calories",
+    price: "30",
+    img: item1,
+  },
+  {
+    id: 1,
+    title: "Craze",
+    description: "30 Calories",
+    price: "23",
+    img: item2,
+  },
+  {
+    id: 2,
+    title: "Watermelon",
+    description: "30 Calories",
+    price: "10",
+    img: item3,
+  },
+  {
+    id: 3,
+    title: "122 Calories",
+    description: "30 Calories",
+    price: "23",
+    img: item4,
+  },
+];
+
+export default lists;
