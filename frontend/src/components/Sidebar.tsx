4c0594f6ec63f3625e355b3ed6b32ccd7018a481
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 14:13:25 -0400

    updated component

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,13 @@
+import { TiArrowBack, FcClearFilters } from "react-icons/all";
+
 function Sidebar() {
     return (
-        <div className='bg-white fixed right-0 h-full w-[375px]'>
-            
+        <div className='bg-white fixed right-0 h-full w-[400px] top-0 z-20 drop-shadow-lg'>
+            <div className="flex items-center justify-between px-8 mt-6">
+                <TiArrowBack cursor="pointer" size={25} />
+                <h2 className="text-xl font-semibold text-textColor">Cart</h2>
+                <button className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md hover:shadow-md  cursor-pointer text-textColor text-base active:scale-75 duration-200">Clear <FcClearFilters /></button>
+            </div>
         </div>
     );
 }
