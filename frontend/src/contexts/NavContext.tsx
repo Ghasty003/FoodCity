f409be7c397987b28ed19a155824a004b72d51c3
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 19:37:25 -0400

    added close nav function

--- frontend/src/contexts/NavContext.tsx
+++ frontend/src/contexts/NavContext.tsx
@@ -12,6 +12,11 @@ export const NavContextProvider: React.FC<Prop> = ({ children }) => {
         {right: "0", opacity: 1}
     ]
 
+    const sideBarSlideIn = [
+        {right: "0", opacity: 1},
+        {right: "-100%", opacity: 0}
+    ]
+
     const sideBarAnimationOption = {
         duration: 600,
         iterations: 1,
@@ -25,9 +30,13 @@ export const NavContextProvider: React.FC<Prop> = ({ children }) => {
         sidebar.current.classList.replace("-right-full", "right-0");
     }
 
+    const closeNav = () => {
+        sidebar.current.animate(sideBarSlideIn, sideBarAnimationOption);
+        sidebar.current.classList.replace("right-0", "-right-full");
+    }
 
     return (
-        <NavContext.Provider value={{openNav, sidebar}}>
+        <NavContext.Provider value={{openNav, sidebar, closeNav}}>
             { children }
         </NavContext.Provider>
     )
