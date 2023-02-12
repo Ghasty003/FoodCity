522dd05eb2051a732892842f4d73204707c91f23
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:42:58 -0400

    added ice cream data

---
+++ frontend/src/data/icecream-data.ts
@@ -0,0 +1,36 @@
+import img1 from "../assets/i2.png";
+import img2 from "../assets/i3.png";
+import img3 from "../assets/i5.png";
+import img4 from "../assets/i6.png";
+
+
+export default [
+    {
+        id: 0,
+        title: "Ice-cream 1",
+        description: "20 calories",
+        price: "10",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Ice-cream 2",
+        description: "10 calories",
+        price: "8",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Ice-cream 3",
+        description: "8 calories",
+        price: "5",
+        img: img3
+    },
+    {
+        id: 3,
+        title: "Ice-cream 4",
+        description: "10 calories",
+        price: "10",
+        img: img4
+    }
+]
