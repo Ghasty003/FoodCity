3423225018a7cce68e8a6642bae0a875b1e643a9
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 11:43:45 -0400

    added chicken data

---
+++ frontend/src/data/chicken-data.ts
@@ -0,0 +1,43 @@
+import img1 from "../assets/food6.png";
+import img2 from "../assets/food5.png";
+import img3 from "../assets/food4.png";
+import img4 from "../assets/food2.png";
+import img5 from "../assets/food1.png";
+
+export default [
+    {
+        id: 0,
+        title: "Chicken 1",
+        description: "599 calories",
+        price: "50",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Chicken 2",
+        description: "456 calories",
+        price: "50",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Chicken 3",
+        description: "441 calories",
+        price: "40",
+        img: img3
+    },
+    {
+        id: 3,
+        title: "Chicken 4",
+        description: "56 calories",
+        price: "22",
+        img: img4
+    },
+    {
+        id: 4,
+        title: "Chicken 5",
+        description: "45 calories",
+        price: "50",
+        img: img5
+    }
+]
