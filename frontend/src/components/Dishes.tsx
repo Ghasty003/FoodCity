08be6f57ee02590c66d53fb97b55c86761361fdc
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 11:08:14 -0400

    completed header layout

--- frontend/src/components/Dishes.tsx
+++ frontend/src/components/Dishes.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function Dishes() {
     return (
-        <div>
-            Dishes
+        <div className='bg-cartNumBg w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+            <div className='w-10 h-10 rounded-full shadow-lg bg-white group-hover:bg-white flex items-center justify-center'>
+                
+            </div>
+            <p className='text-sm text-white group-hover:text-white'>Chicken</p>
         </div>
     );
 }
--- frontend/src/components/DishesHeader.tsx
+++ frontend/src/components/DishesHeader.tsx
@@ -11,9 +11,49 @@ function DishesHeader() {
                 <p className='w-32 h-1 rounded-xl bg-orange-500'></p>
             </div>
 
-            <div className='w-full relative mt-16 overflow-y-hidden overflow-x-auto'>
-                <div className='flex mt-4'>
-                    <Dishes />
+            <div className='w-full relative mt-10 overflow-y-hidden overflow-x-auto'>
+                <div className='flex justify-center items-center gap-14'>
+                    <div className='bg-cartNumBg w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-white group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-white group-hover:text-white'>Chicken</p>
+                    </div>
+
+                    <div className='bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-textColor group-hover:text-white'>Curry</p>
+                    </div>
+
+                    <div className='bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-textColor group-hover:text-white'>Rice</p>
+                    </div>
+
+                    <div className='bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-textColor group-hover:text-white'>Fish</p>
+                    </div>
+
+                    <div className='bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-textColor group-hover:text-white'>Fruits</p>
+                    </div>
+
+                    <div className='bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg'>
+                        <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
+                            
+                        </div>
+                        <p className='text-sm text-textColor group-hover:text-white'>Ice creams</p>
+                    </div>
                 </div>
             </div>
         </div>
