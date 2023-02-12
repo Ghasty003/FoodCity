5a88fe244ffed0ab40a9ffddb914d691cf66f77f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:25:56 -0400

    added fish component

---
+++ frontend/src/components/Fish.tsx
@@ -0,0 +1,16 @@
+import data from "../data/fish-data";
+import Card from "./Card";
+
+function Fish() {
+    return (
+        <div className="flex flex-wrap justify-around mt-10 gap-8">
+            {
+                data.map(d => (
+                    <Card key={d.id} title={d.title} price={d.price} description={d.description} img={d.img} />
+                ))
+            }
+        </div>
+    );
+}
+
+export default Fish;
