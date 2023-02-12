3102705e5bb6eca2cc9a7be892b2eeba537eb8cf
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:34:07 -0400

    added fruit data

---
+++ frontend/src/data/fruit-data.ts
@@ -0,0 +1,44 @@
+import img1 from "../assets/f2.png";
+import img2 from "../assets/f3.png";
+import img3 from "../assets/f5.png";
+import img4 from "../assets/f6.png";
+import img5 from "../assets/f7.png";
+
+
+export default [
+    {
+        id: 0,
+        title: "Fruit 1",
+        description: "20 calories",
+        price: "10",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Fruit 2",
+        description: "10 calories",
+        price: "12",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Fruit 3",
+        description: "15 calories",
+        price: "5",
+        img: img3
+    },
+    {
+        id: 3,
+        title: "Fruit 4",
+        description: "20 calories",
+        price: "20",
+        img: img4
+    },
+    {
+        id: 4,
+        title: "Fruit 5",
+        description: "10 calories",
+        price: "10",
+        img: img5
+    }
+]
