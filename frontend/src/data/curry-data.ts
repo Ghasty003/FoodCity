c74e5bfc262cce453cf277893ef5fac665fa24bf
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 12:37:18 -0400

    added curry data

---
+++ frontend/src/data/curry-data.ts
@@ -0,0 +1,27 @@
+import img1 from "../assets/food7.png";
+import img2 from "../assets/food9.png";
+import img3 from "../assets/food12.png";
+
+export default [
+    {
+        id: 0,
+        title: "Curry 1",
+        description: "50 calories",
+        price: "40",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Curry 2",
+        description: "70 calories",
+        price: "60",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Curry 3",
+        description: "30 calories",
+        price: "30",
+        img: img3
+    }
+]
