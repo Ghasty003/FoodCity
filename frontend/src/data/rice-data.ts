c5afd9510392ee04516302f4507308f0702b28c2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:14:26 -0400

    added rice data

---
+++ frontend/src/data/rice-data.ts
@@ -0,0 +1,44 @@
+import img1 from "../assets/r1.png";
+import img2 from "../assets/r2.png";
+import img3 from "../assets/r3.png";
+import img4 from "../assets/r4.png";
+import img5 from "../assets/r5.png";
+
+
+export default [
+    {
+        id: 0,
+        title: "Rice 1",
+        description: "99 calories",
+        price: "50",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Rice 2",
+        description: "90 calories",
+        price: "40",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Rice 3",
+        description: "50 calories",
+        price: "20",
+        img: img3
+    },
+    {
+        id: 3,
+        title: "Rice 4",
+        description: "59 calories",
+        price: "35",
+        img: img4
+    },
+    {
+        id: 4,
+        title: "Rice 5",
+        description: "89 calories",
+        price: "50",
+        img: img5
+    }
+]
