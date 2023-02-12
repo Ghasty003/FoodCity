975b1e31773b671e558b8f2fc57925d3daad2588
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:21:11 -0400

    added fish data

---
+++ frontend/src/data/fish-data.ts
@@ -0,0 +1,43 @@
+import img1 from "../assets/fi2.png";
+import img2 from "../assets/fi3.png";
+import img3 from "../assets/fi4.png";
+import img4 from "../assets/fi5.png";
+import img5 from "../assets/fish.png";
+
+export default [
+    {
+        id: 0,
+        title: "Fish 1",
+        description: "30 calories",
+        price: "50",
+        img: img1
+    },
+    {
+        id: 1,
+        title: "Fish 2",
+        description: "40 calories",
+        price: "70",
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Fish 3",
+        description: "20 calories",
+        price: "30",
+        img: img3
+    },
+    {
+        id: 3,
+        title: "Fish 4",
+        description: "60 calories",
+        price: "80",
+        img: img4
+    },
+    {
+        id: 4,
+        title: "Fish 5",
+        description: "40 calories",
+        price: "60",
+        img: img5
+    }
+]
