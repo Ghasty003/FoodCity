af0992a0cdc6f7ca01665245fcdddddbb7b0b919
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 12:37:48 -0400

    added Curry component

---
+++ frontend/src/components/Curry.tsx
@@ -0,0 +1,18 @@
+import data from "../data/curry-data";
+import Card from "./Card";
+
+
+function Curry() {
+    return (
+        <div className="flex flex-wrap justify-around mt-10 gap-8">
+            {
+                data.map(d => (
+                    <Card key={d.id} title={d.title} description={d.description} price={d.price} img={d.img} />
+                ))
+            }
+        </div>
+    )
+}
+
+
+export default Curry;
