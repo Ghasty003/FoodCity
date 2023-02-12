83c0ea591a04a629ac0da1e48b192945fe02d5bf
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 12:59:06 -0400

    added renderer provider

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,14 +2,17 @@ import DishesHeader from '../components/DishesHeader';
 import NavBar from '../components/NavBar';
 import ProductHeader from '../components/ProductHeader';
 import Products from '../components/Products';
+import { RendererContextProvider } from '../contexts/RendererContext';
 
 function Home() {
     return (
         <div className='px-14'>
-            <NavBar />
-            <ProductHeader />
-            <Products />
-            <DishesHeader />
+            <RendererContextProvider>
+                <NavBar />
+                <ProductHeader />
+                <Products />
+                <DishesHeader />
+            </RendererContextProvider>
         </div>
     );
 }
