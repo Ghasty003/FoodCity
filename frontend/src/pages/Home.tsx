44814e90dfde559d1a6cb8047b9df1588fb3ccc8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-12 13:54:36 -0400

    rendered sidebar component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import DishesHeader from '../components/DishesHeader';
 import NavBar from '../components/NavBar';
 import ProductHeader from '../components/ProductHeader';
 import Products from '../components/Products';
+import Sidebar from '../components/Sidebar';
 import { RendererContextProvider } from '../contexts/RendererContext';
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
         <div className='px-14'>
             <RendererContextProvider>
                 <NavBar />
+                <Sidebar />
                 <ProductHeader />
                 <Products />
                 <DishesHeader />
