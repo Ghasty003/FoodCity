be9aff9ce2983e16b162ed77615dcb07803da37b
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 02:33:18 -0400

    added cart provider

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,21 +3,24 @@ import NavBar from '../components/NavBar';
 import ProductHeader from '../components/ProductHeader';
 import Products from '../components/Products';
 import Sidebar from '../components/Sidebar';
+import { CartContextProvider } from '../contexts/CartContext';
 import { NavContextProvider } from '../contexts/NavContext';
 import { RendererContextProvider } from '../contexts/RendererContext';
 
 function Home() {
     return (
         <div className='px-14'>
-            <RendererContextProvider>
-                <NavContextProvider>
-                    <NavBar />
-                    <Sidebar />
-                    <ProductHeader />
-                    <Products />
-                    <DishesHeader />
-                </NavContextProvider>
-            </RendererContextProvider>
+            <CartContextProvider>
+                <RendererContextProvider>
+                    <NavContextProvider>
+                        <NavBar />
+                        <Sidebar />
+                        <ProductHeader />
+                        <Products />
+                        <DishesHeader />
+                    </NavContextProvider>
+                </RendererContextProvider>
+            </CartContextProvider>
         </div>
     );
 }
