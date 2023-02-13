645460f799ca434d424844b63661ac8e045494f7
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 02:31:25 -0400

    added click event to items

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import { useContext } from 'react';
 import { BsCartPlusFill } from "react-icons/bs";
+import CartContext from '../contexts/CartContext';
 import { CardProps } from "../types/types";
 
-function Card({price, title, description, img}: CardProps) {
+function Card({price, title, description, img, id}: CardProps) {
+
+    const { addItemToCart } = useContext(CartContext);
+
     return (
         <div className='bg-cardOverlay backdrop-blur-lg hover:drop-shadow-lg h-40 w-[280px] rounded-xl relative p-4 mx-5'>
             <img className='w-[128px] hover:scale-[1.2] duration-300 absolute -top-5 left-4' src={img} alt="image" />
             <div className='flex flex-col items-end justify-end'>
-                <span className='bg-red-600 p-2 mt-2 rounded-full text-center cursor-pointer'>
+                <span onClick={() => addItemToCart(title, description, price, img, id)} className='bg-red-600 p-2 mt-2 rounded-full text-center cursor-pointer'>
                     <BsCartPlusFill className='text-white text-lg' />
                 </span>
                 <div className='absolute bottom-2 flex flex-col items-end '>
