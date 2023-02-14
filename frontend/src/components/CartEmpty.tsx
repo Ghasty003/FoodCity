ecc908a6c4deed28da13fe3626de57899beb5f02
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 20:31:41 -0400

    updated layout

--- frontend/src/components/CartEmpty.tsx
+++ frontend/src/components/CartEmpty.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CartEmpty() {
     return (
-        <div className='w-full h-full bg-slate-300 text-2xl text-textColor font-semibold text-center'>
+        <div className='w-full h-full flex items-center justify-center text-2xl text-textColor font-semibold text-center'>
             Add some items to your cart
         </div>
     );
