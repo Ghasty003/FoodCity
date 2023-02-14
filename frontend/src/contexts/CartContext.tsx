393e5ccc0a3f725bc86657778269ad699b5e7048
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 20:13:07 -0400

    updated add to cart function

--- frontend/src/contexts/CartContext.tsx
+++ frontend/src/contexts/CartContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { CartContextProps, Prop } from '../types/types';
+import React, { createContext, useState } from 'react';
+import { CardProps, CartContextProps, Prop } from '../types/types';
 
 
 const CartContext = createContext<CartContextProps>(null!);
@@ -7,12 +7,27 @@ const CartContext = createContext<CartContextProps>(null!);
 
 export const CartContextProvider: React.FC<Prop> = ({ children }) => {
 
+    const [cartItem, setCartItem] = useState<CardProps[]>([]);
+
     const addItemToCart = (title: string, description: string, price: string, img: string, id: number) => {
-        console.log(title, description, img, price, id);
+        if (cartItem.find(item => item.title == title && item.description == description)) {
+            console.log("item already exists");
+            return;
+        }
+        
+        setCartItem(prev => (
+            [...prev, {
+                id,
+                description,
+                title,
+                img,
+                price
+            }]
+        ))
     }
 
     return (
-        <CartContext.Provider value={{addItemToCart}}>
+        <CartContext.Provider value={{addItemToCart, cartItem, setCartItem}}>
             { children }
         </CartContext.Provider>
     )
