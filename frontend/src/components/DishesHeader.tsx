bc9782f65dacf0da5e3e5666a416e0b9f43b4726
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 21:30:45 -0400

    updated styles

--- frontend/src/components/DishesHeader.tsx
+++ frontend/src/components/DishesHeader.tsx
@@ -6,7 +6,7 @@ const Div: React.FC<{text: string}> = ({text}) => {
     const { updateRenderer } = useContext(RendererContext);
 
     return (
-        <div onClick={() => updateRenderer(text)} className='group bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg duration-300 active:scale-75'>
+        <div onClick={() => updateRenderer(text)} className='group mx-10 my-6 bg-card w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-xl flex flex-col gap-3 items-center justify-center hover:bg-cartNumBg duration-300 active:scale-75'>
             <div className='w-10 h-10 rounded-full shadow-lg bg-cartNumBg group-hover:bg-white flex items-center justify-center'>
                 <FaHamburger className="text-white text-lg group-hover:text-textColor" />
             </div>
@@ -26,8 +26,8 @@ function DishesHeader() {
                 <p className='w-32 h-1 rounded-xl bg-orange-500'></p>
             </div>
 
-            <div className='w-full relative mt-10'>
-                <div className='flex justify-center items-center gap-14'>
+            <div className='w-full flex justify-center relative mt-10 overflow-x-auto scrollbar-hide sm:overflow-x-hidden'>
+                <div className='flex w-full ml-8'>
                     <Div text={"Chicken"} />
                     <Div text="Curry" />
                     <Div text="Rice" />
