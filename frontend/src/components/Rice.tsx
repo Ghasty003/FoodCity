609f21457d3cf0bd540a2c9d2f346142a6e165e8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-02-13 21:40:48 -0400

    added id

--- frontend/src/components/Rice.tsx
+++ frontend/src/components/Rice.tsx
@@ -6,7 +6,7 @@ function Rice() {
         <div className="flex flex-wrap justify-around mt-10 gap-8">
             {
                 data.map(d => (
-                    <Card key={d.id} title={d.title} price={d.price} description={d.description} img={d.img} />
+                    <Card key={d.id} id={d.id} title={d.title} price={d.price} description={d.description} img={d.img} />
                 ))
             }
         </div>
